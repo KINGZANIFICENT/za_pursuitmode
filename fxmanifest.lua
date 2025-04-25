21ba367441e2629099f6e2808944a090d2364959
KINGZANIFICENT <chefangel.vargas23@gmail.com>
2025-04-25 13:35:20 -0400

    release

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'KingZa'
+description 'pursuit mode'
+version '1.0.0'
+lua54 'yes'
+
+client_scripts { 
+    "config.lua", 
+    "handling.lua", 
+    "client.lua" 
+}
